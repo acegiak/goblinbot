c0e5b46f5396768d7607054d7acbabea6eddf8d8
Ashton McAllan <acegiak@lovelace>
2017-08-16 12:08:44 +0930

    adding permission system and fixing some voting issues

--- goblinbot.py
+++ goblinbot.py
@@ -3,44 +3,67 @@ import re, socket, time, sys, random, json, pickle
 
 
 class goblinbot:
-    def processCommand(self,sender,command):
+    def processCommand(self,sender,command, badges):
         part = command.split(" ")
+
+        if not hasattr(self,'permissions') and command[1] == "!":
+            return "permissions have not been configured"
+        badges.append("global")
+        myPermissions = []
+
+        allowed = []
+        for level in self.permissions.keys():
+            if part[0].lower() in self.permissions[level]:
+                allowed.append(level)
+        permitted = False
+        for badge in badges:
+            if badge in allowed:
+                permitted = True
+        if len(allowed)> 0 and not permitted:
+            return "The "+part[0]+" command can only be used by the following user types: "+(",".join(allowed))
+
         if part[0] == "!subname":
+            if not hasattr(self,'namelists'):
+                return "No namelists are configured"
             if len(part)<3:
                 return "Not enough arguments. Please enter !getname game("+(', '.join(self.namelists))+") yoursuggestion"
-
             if part[1].upper() not in self.namelists:
                 return "Name list invalid. Please enter !subname game("+(', '.join(self.namelists))+") yoursuggestion"
             if part[1].upper() not in self.names:
                 self.names[part[1].upper()] = []
             self.names[part[1].upper()].append(" ".join(part[2:]))
+            #save the updated list
+            pickle.dump(self.names, open("pickles/names.pickle", "wb"))
             return "Your name "+part[2]+" has been submitted to the "+part[1]+" namelist."
 
         if part[0] == "!listnames":
+            if not hasattr(self,'namelists'):
+                return "No namelists are configured"
             if len(part)<2:
                 return "Not enough arguments. Please enter !getname game("+(', '.join(self.namelists))+")"
             if part[1].upper() not in self.namelists:
                 return "Name list invalid. Please enter !getname game("+(', '.join(self.namelists))+")"
             if part[1].upper() not in self.names or len(self.names[part[1].upper()])<1:
                 return "No names for that list"
-            self.send(', '.join(self.names[part[1].upper()])+"\r\n")
+            return ', '.join(self.names[part[1].upper()])+"\r\n"
 
         if part[0] == "!randomname":
+            if not hasattr(self,'namelists'):
+                return "No namelists are configured"
             if len(part)<2:
                 return "Not enough arguments. Please enter !getname game("+(', '.join(self.namelists))+")"
             if part[1].upper() not in self.namelists:
                 return "Name list invalid. Please enter !getname game("+(', '.join(self.namelists))+")"
             if part[1].upper() not in self.names or len(self.names[part[1].upper()])<1:
                 return "No names for that list"
-            self.send(random.choice(self.names[part[1].upper()])+"\r\n"))
+            return random.choice(self.names[part[1].upper()])
 
-        pickle.dump(self.names, open("pickles/names.pickle", "wb"))
 
         if part[0] == "!roll":
             if len(part) < 2:
                 return "Not enough arguments. Please enter !roll rollcode(1d6, 2d4+1, etc)"
             for part in part[1:]:
-                match = re.match("([0-9]+)[Dd]([0-9]+)([+\-][0-9]+)?")
+                match = re.match("([0-9]+)[Dd]([0-9]+)([+\-][0-9]+)?",part)
                 if not match:
                     return "rollcode must be in the format 1d1+1"
                 else:
@@ -57,55 +80,87 @@ class goblinbot:
                         if match.group(3)[0] == "-":
                             output.append(match.group(3))
                             total -= int(match.group(3)[1:])
-                    return (", ".join(output))+" = "+str(total)
+                    return sender+" rolled "+(", ".join(output))+" = "+str(total)
 
-	if part[0] == "!vote":
-		if len(part) < 3:
-			return "Not enough arguments. Please enter !vote pollname option"
-		if part[1].lower() not in self.polls:
-			return "There is no poll called "+part[1]
-		#allow for either vote options that are from a specified list or freeform
-		if self.polls[part[1].lower()] != "*" and part[2].lower() not in self.polls[part[1].lower()]:
-			return "The poll "+part[1]+" does not allow the option "+part[2]+". Options are:"+(", ".join(self.polls[part[1].lower()]))
-		#build the structure if it's not already there
-		if part[1].lower() not in self.votes:
-			self.votes[part[1].lower()] = {}
-		if part[2].lower() not in self.votes[part[1].lower()]:
-			self.votes[part[1].lower()][part[2].lower()] = []
-		#remove their vote from any existing options
-		for option in self.votes[part[1].lower()]:
-			option.remove(sender)
-		#put their vote in their new choice
-		self.votes[part[1].lower()][part[2].lower()].append(sender)
+        if part[0] == "!vote":
+            if not hasattr(self,'polls'):
+                return "No polls are configured"
+            if len(part) < 3:
+                return "Not enough arguments. Please enter !vote pollname option"
+            if part[1].lower() not in self.polls:
+                return "There is no poll called "+part[1]
+            #allow for either vote options that are from a specified list or freeform
+            if self.polls[part[1].lower()] != "*" and part[2].lower() not in self.polls[part[1].lower()]:
+                return "The poll "+part[1]+" does not allow the option "+part[2]+". Options are:"+(", ".join(self.polls[part[1].lower()]))
+            #build the structure if it's not already there
+            if self.votes == None:
+                self.votes = {}
+            if part[1].lower() not in self.votes:
+                self.votes[part[1].lower()] = {}
+            if part[2].lower() not in self.votes[part[1].lower()]:
+                self.votes[part[1].lower()][part[2].lower()] = []
+            #remove their vote from any existing options
+            for option in self.votes[part[1].lower()]:
+                if sender in self.votes[part[1].lower()][option]:
+                    self.votes[part[1].lower()][option].remove(sender)
+            #put their vote in their new choice
+            self.votes[part[1].lower()][part[2].lower()].append(sender)
+            return sender+" voted for "+part[1].lower()+": "+part[2].lower()
 
-	if part[0] == "!results":
-		output = []
-		if part[1].lower() not in self.votes:
-                        self.votes[part[1].lower()] = {}
-		for option in self.votes[part[1].lower()].keys():
-                        output.append(option+": "+str(len(self.votes[part[1].lower()][option])))
-		return "\r\n".join(output)
+        if part[0] == "!results":
+            if not hasattr(self,'polls'):
+                return "No polls are configured"
+            if not hasattr(self,'votes'):
+                return "No votes have been submitted"
+            if len(part) <2:
+                return "Not enough arguments. Please enter !results pollname"
+            if part[1].lower() not in self.polls:
+                return part[1].lower()+" is not a current poll"
+            if part[1].lower() not in self.votes:
+                return part[1].lower()+" has no submitted votes"
+            output = []
+            for option in self.votes[part[1].lower()].keys():
+                output.append(option+": "+str(len(self.votes[part[1].lower()][option])))
+            return "\r\n".join(output)
 
-        #This just returns whatever is listed against the command in the json
-        if part[0] in self.commands:
+        if part[0] == "!reload":
+            self.loadConfig()
+            return "reloaded"
+
+            #This just returns whatever is listed against the command in the json
+        if hasattr(self,'commands') and part[0].lower() in self.commands:
             return self.commands[part[0]]
 
 
     def send(self,message):
-        self.irc.send(('PRIVMSG '+self.channel+' : '+message+" \r\n").encode("utf-8"))
+        if message is None:
+            print "MESSAGE WAS NONE"
+            return
+        print "SENDING:"+message
+        self.irc.send(("PRIVMSG "+self.channel+" : "+message+" \r\n").encode("utf-8"))
+
+
+    def loadConfig(self):
+        json_data=open("config.json").read()
+        config = json.loads(json_data)
+        self.name = config['name']
+        self.password = config['oauth']
+        self.channel = config['channel']
+
+        if 'namelists' in config:
+            self.namelists = config['namelists']
+        if 'customcommands' in config:
+            self.commands = config['customcommands']
+        if 'polls' in config:
+            self.polls = config['polls']
+        if 'permissions' in config:
+            self.permissions = config['permissions']
 
     def main(self):
         self.waitinglist = []
-        json_data=open("credentials.json").read()
-        creds = json.loads(json_data)
-        self.name = creds['name']
-        self.password = creds['oauth']
-        self.channel = creds['channel']
+        self.loadConfig()
+        self.votes = {}
 
-        self.namelists = creds['namelists']
-        self.commands = creds['commands']
-        self.polls = creds['polls']
-	self.votes = {}
         try:
             self.names = pickle.load(open("pickles/names.pickle", "rb"))
         except (OSError, IOError) as e:
@@ -144,20 +199,25 @@ class goblinbot:
 
                         if(line[0]=="PING"):
                             s.send("PONG %s\r\n" % line[1].encode("utf-8"))
-                        breakup = re.split('^:(.*?)!.*?PRIVMSG (.*?) :(.*?)$',line)
+                        breakup = re.split('^(@badges=.*?;)?.*?:(.*?)!.*?PRIVMSG (.*?) :(.*?)$',line)
                         for index, item in enumerate(breakup):
                             print index, item
 
                         if len(breakup) > 1:
-                            if breakup[2] != self.name:
-                                w = breakup[2]
-                            elif breakup[1] != "":
+                            if breakup[3] != self.name:
+                                w = breakup[3]
+                            elif breakup[2] != "":
                                 w = "twitchpm"
 
                             namesplit = [self.name[0:i].lower() for i in range(3, len(self.name) + 1)]
-                            bwo = breakup[3].lstrip()
+                            bwo = breakup[4].lstrip()
 
-                            sender = breakup[1]
+                            sender = breakup[2]
+                            badges = []
+                            if breakup[1] is not None:
+                                badges = breakup[1][8:].split(",")
+                            for badge in range(0,len(badges)):
+                                badges[badge] = re.sub("/[0-9]+;?$","",badges[badge])
 
                             breakupsplit = re.split("(, |: )",bwo,1)
                             bettersplit = re.match("^([^ ]+) ?(, |: )(.+)$",bwo)
@@ -173,10 +233,13 @@ class goblinbot:
 
                             #Here's where we actually send the command off to be processed
                             if words[0]=="!":
-                                self.send(self.processCommand(sender,words))
+                                self.send(self.processCommand(sender,words,badges))
 
                         if line.find("hostname") != -1:
                             self.irc.send ( 'JOIN '+self.channel+'\r\n'.encode("utf-8"))
+                        if line.find("GLHF") != -1:
+                            self.irc.send ( 'CAP REQ :twitch.tv/membership\r\n'.encode("utf-8"))
+                            self.irc.send ( 'CAP REQ :twitch.tv/tags\r\n'.encode("utf-8"))
                         if line.find("his nickname is registered") != -1:
                             self.irc.send ( 'PRIVMSG NickServ : identify lovessocks\r\n'.encode("utf-8") )
                         if line.find("ou have not registered") != -1:
@@ -185,8 +248,10 @@ class goblinbot:
                             stayalive = False
                             time.sleep(6)
                         if line.find ( 'PING' ) != -1:
-                            self.irc.send ( 'PONG ' + line.split() [ 1 ] + '\r\n'.encode("utf-8") )
+                            response = 'PONG ' + line.split() [ 1 ] + '\r\n'.encode("utf-8")
+                            self.irc.send ( response )
                             print "pinged"
+                            print "responded:"+response
                         if line.find ( 'KICK' ) != -1:
                             self.irc.send ( 'JOIN '+self.channel+'\r\n'.encode("utf-8") )
                         if(len(self.waitinglist) > 0):
